acaa995960a0b8b538cdd4ccd025f95e9bd8a0c9
Suharsh Nagasampagi <suharsh329@gmail.com>
2025-05-31 00:11:38 +1000

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import routes from './routes';
 import limiter from './middleware/rate-limiter';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
